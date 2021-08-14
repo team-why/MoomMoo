b6eef8a9d48b0a34fbbb291f79e5566cb7bbac41
h3yon <khykhy1006@gmail.com>
2021-08-14 18:11:11 +0900

    add feat:naver login

---
+++ src/main/java/com/hwhy/moommoo/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,53 @@
+package com.hwhy.moommoo.config.auth.dto;
+
+import com.hwhy.moommoo.domain.user.entity.Role;
+import com.hwhy.moommoo.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture){
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+                                     Map<String ,Object> attributes) {
+            return ofNaver("id", attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String,Object> attributes) {
+        Map<String,Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+
+    public User toEntity(){
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
---
+++ src/main/java/com/hwhy/moommoo/domain/user/entity/Role.java
@@ -0,0 +1,16 @@
+package com.hwhy.moommoo.domain.user.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
