8e1f8fb603adc2717f4a169eb8d179a1fe5af967
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-14 15:16:35 +0900

    Gender Enum 추가

---
+++ src/main/java/com/hwhy/moommoo/global/code/Gender.java
@@ -0,0 +1,21 @@
+package com.hwhy.moommoo.global.code;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Gender implements MoomMooEnumerable{
+    MALE(0),
+    FEMALE(1);
+
+    private final int value;
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+}
---
+++ src/main/java/com/hwhy/moommoo/global/code/MoomMooEnumerable.java
@@ -0,0 +1,7 @@
+package com.hwhy.moommoo.global.code;
+
+public interface MoomMooEnumerable {
+    int getValue();
+
+    String getKey();
+}
