28f83036b2e1cb74c808c7b791ae51b6fe59f882
h3yon <khykhy1006@gmail.com>
2021-09-12 16:22:03 +0900

    feat: PROD Initial commit

---
+++ src/main/java/com/hwhy/moommoo/global/exception/JsonException.java
@@ -0,0 +1,15 @@
+package com.hwhy.moommoo.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class JsonException extends BaseException {
+
+    public JsonException(Status status) {
+        super(status);
+    }
+
+    public JsonException(Integer code, String message) {
+        super(code, message);
+    }
+}
